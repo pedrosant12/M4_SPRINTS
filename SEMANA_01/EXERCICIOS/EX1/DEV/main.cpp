1c33831677322c592965e2f884736c7ff3d1cea6
pedrosant12 <pedro.oliveira@sou.inteli.edu.br>
2022-10-23 22:02:22 -0300

    a

--- SEMANA_01/EXERCICIOS/EX1/DEV/main.cpp
+++ SEMANA_01/EXERCICIOS/EX1/DEV/main.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
 #include <string>
 
-// 1 -  Faça uma função que recebe uma certa medida e ajusta ela percentualmente 
-// entre dois valores mínimo e máximo e retorna esse valor
+//g++ hello.cpp -o hello
+//./hello
 
-// 2 - Faça uma função que simule a leitura de um sensor lendo o 
+using namespace std;
+
+// 1 — Faça uma função que recebe uma certa medida e ajusta ela percentualmente
+// entre dois valores mínimo e máximo e retorna esse valor
+int perc(){
+    int perc;
+    perc = 5;
+    cout << perc <<endl;
+
+
+    return 0;
+
+}
+// 2 — Faça uma função que simule a leitura de um sensor lendo o
 // valor do teclado ao final a função retorna este valor
 
-// 3 - Faça uma função que armazena uma medida inteira qualquer 
-// em um vetor fornecido. Note que como C não possui vetores 
+// 3 — Faça uma função que armazena uma medida inteira qualquer
+// em um vetor fornecido. Note que como C não possui vetores
 // nativos da linguagem, lembre-se que você precisa passar o 
 // valor máximo do vetor assim como a última posição preenchida
-// Evite também que, por acidente, um valor seja escrito em 
-// uma área de memória fora do vetor
+// Evite também que, por acidente, um valor seja escrito
+// numa área de memória fora do vetor
 
 
 
-// 4 - Faça uma função que recebe um vetor com 4 posições que contém 
+// 4 — Faça uma função que recebe um vetor com 4 posições que contém
 // o valor da distância de um pequeno robô até cada um dos seus 4 lados.
 // A função deve retornar duas informações: A primeira é a direção 
 // de maior distância ("Direita", "Esquerda", "Frente", "Tras") e a 
@@ -25,21 +38,21 @@
 
 
 
-// 5 - Faça uma função que pergunta ao usuário se ele deseja continuar o mapeamento e 
+// 5 — Faça uma função que pergunta ao usuário se ele deseja continuar o mapeamento e
 // retorna verdadeiro ou falso
 
 
-// 6 - A função abaixo (que está incompleta) vai "dirigindo" virtualmente um robô 
-// e através de 4 sensores em cada um dos 4 pontos do robo ("Direita", "Esquerda", 
+// 6 — A função abaixo (que está incompleta) vai "dirigindo" virtualmente um robô
+// e através de 4 sensores em cada um dos 4 pontos do robo ("Direita", "Esquerda",
 // "Frente", "Tras"). 
 //      A cada passo, ele verifica as distâncias aos objetos e vai mapeando o terreno 
 // para uma movimentação futura. 
-//      Ele vai armazenando estas distancias em um vetor fornecido como parâmetro 
-// e retorna a ultima posicao preenchida do vetor.
-//      Esta função deve ir lendo os 4 sensores até que um comando de pare seja enviado 
+//      Ele vai armazenando estas distâncias em um vetor fornecido como parâmetro
+// e retorna a última posicao preenchida do vetor.
+//      Esta função deve ir lendo os 4 sensores até que um comando de pare seja enviado
 //
 //      Para simular os sensores e os comandos de pare, use as funções já construídas 
-// nos ítens anteriores e em um looping contínuo até que um pedido de parada seja 
+// nos ítens anteriores e em um looping contínuo até que um pedido de parada seja
 // enviado pelo usuário. 
 //
 //      Complete a função com a chamada das funções já criadas
@@ -85,3 +98,7 @@ int main(int argc, char** argv) {
 	
 	return 0;
 }
+
+
+
+
